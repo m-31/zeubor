242c25f7ef4bcc3f9ec63cf70bd7b567dedfbe67
Michael Meyling <14306799+m-31@users.noreply.github.com>
2023-05-20 12:47:10 +0200

    trying to get conda running

--- README.md
+++ README.md
@@ -6,6 +6,6 @@
 ```bash
 conda create -p ./env python=3.10
 conda activate ./env
-env/bin/pip install pygame
+env/bin/pip install pygame numpy
 conda env export > environment.yml
 ```
--- main.py
+++ main.py
@@ -29,37 +29,26 @@ class Ball:
             focal_length // z
         )
 
-def init():
-    # Generate balls
-    global balls
-    balls = [Ball() for _ in range(100)]  # 100 balls
+# Generate balls
+balls = [Ball() for _ in range(100)]  # 100 balls
 
-    # Camera settings
-    global camera_position, focal_length
-    camera_position = np.array([0, 0, -20])
-    focal_length = 200
+# Camera settings
+camera_position = np.array([0, 0, -20])
+focal_length = 200
 
-def run():
-    # Game loop
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        screen.fill((0, 0, 0))
-        for ball in balls:
-            projection = ball.project(camera_position, focal_length)
-            if projection is not None:
-                pygame.draw.circle(screen, (255, 255, 255), (projection[0], projection[1]), projection[2])
+    screen.fill((0, 0, 0))
+    for ball in balls:
+        projection = ball.project(camera_position, focal_length)
+        if projection is not None:
+            pygame.draw.circle(screen, (255, 255, 255), (projection[0], projection[1]), projection[2])
 
-        pygame.display.flip()
+    pygame.display.flip()
 
-    pygame.quit()
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+pygame.quit()
