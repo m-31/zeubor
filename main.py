d098f8263f3a74fcc02789219f7bf10aee656048
Michael Meyling <14306799+m-31@users.noreply.github.com>
2023-05-20 15:19:08 +0200

    working after tweaking

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import numpy as np
 import random
 
 # Dimensions
-WIDTH, HEIGHT = 2000, 1200
+WIDTH, HEIGHT = 1500, 1000
 
 # Initialize Pygame
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
 
 def rotation_matrix_from_vectors(vec1, vec2):
     """Find the rotation matrix that aligns vec1 to vec2."""
@@ -16,15 +15,16 @@ def rotation_matrix_from_vectors(vec1, vec2):
     v = np.cross(a, b)
     c = np.dot(a, b)
     s = np.linalg.norm(v)
+    if s < 1e-10:  # vectors are parallel
+        return np.eye(3) if c > 0 else -np.eye(3)
     kmat = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
-    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2))
+    rotation_matrix = np.eye(3) + kmat + kmat.dot(kmat) * ((1 - c) / (s ** 2 + 1e-10))
     return rotation_matrix
-
 
 class Ball:
     def __init__(self):
         self.position = np.array([random.uniform(-10, 10) for i in range(3)])
-        self.radius = random.uniform(1, 20)
+        self.radius = random.uniform(1.0, 1.1)
 
     def project(self, camera_position, camera_direction, focal_length):
         """Project the 3D position onto 2D plane."""
@@ -38,24 +38,23 @@ class Ball:
         xp, yp = projected_position[0] + WIDTH // 2, projected_position[1] + HEIGHT // 2
 
         # If the ball is too close or the photo plate is completely inside the ball, cover the photo plate
-        if abs(z) < self.radius or projected_radius > np.sqrt((WIDTH // 2) ** 2 + (HEIGHT // 2) ** 2):
+        if abs(z) < self.radius or projected_radius > np.sqrt((WIDTH // 2)**2 + (HEIGHT // 2)**2):
             return (-WIDTH // 2, -HEIGHT // 2, max(WIDTH, HEIGHT))
 
         # If the ball doesn't touch the photo plate at all, return None
-        if xp + projected_radius < -WIDTH // 2 or xp - projected_radius > WIDTH // 2 or yp + projected_radius < -HEIGHT // 2 or yp - projected_radius > HEIGHT // 2:
-            return None
+        # if xp + projected_radius < -WIDTH // 2 or xp - projected_radius > WIDTH // 2 or yp + projected_radius < -HEIGHT // 2 or yp - projected_radius > HEIGHT // 2:
+        #     return None
 
         # Return position and size
         return (xp, yp, projected_radius)
 
+# Generate balls
+balls = [Ball() for _ in range(100)]  # 100 balls
 
 # Camera settings
 camera_position = np.array([0, 0, -20])
 camera_direction = np.array([0, 0, 1])  # Pointing towards positive z-axis
 focal_length = 200
-
-# Generate balls
-balls = [Ball() for _ in range(100)]  # 100 balls
 
 # Game loop
 running = True
@@ -65,10 +64,14 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
+    number = 0
     for ball in balls:
+        number += 1
         projection = ball.project(camera_position, camera_direction, focal_length)
         if projection is not None:
-            pygame.draw.circle(screen, (255, 255, 255), (projection[0], projection[1]), projection[2])
+            pygame.draw.circle(screen, (255, 255, 255 - 1 * number), (projection[0], projection[1]), projection[2])
+        else:
+            print("Ball " + str(number) + " is too far away or too close to the camera.")
 
     pygame.display.flip()
 
